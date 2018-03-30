b592af4082f27b4df342b37e47935dba3c9a3c99
MofeLee <mofe@me.com>
2018-03-31 02:34:01 +0800

    lint files

--- index.js
+++ index.js
@@ -11,38 +11,38 @@ const wait = (ms)=>new Promise(resolve=>setTimeout(resolve, ms));
 
       await aliyun.invoke('StopInstance', {
         InstanceId: id
-      })
+      });
 
-      await waitUntil(id, 'Stopped')
-      console.log(id, 'stopped')
+      await waitUntil(id, 'Stopped');
+      console.log(id, 'stopped');
 
       await aliyun.invoke('ReplaceSystemDisk', {
         InstanceId: id,
         ImageId: 'ubuntu_16_0402_64_20G_alibase_20180326.vhd',
         KeyPairName: 'mbp15',
-      })
-      await wait(5000)
+      });
+      await wait(5000);
       console.log('replaced dsk');
 
       await aliyun.invoke('StartInstance', {InstanceId: id});
-      await waitUntil(id, 'Running')
+      await waitUntil(id, 'Running');
 
     } catch (e) {
-      console.error(e)
+      console.error(e);
     }
   }));
 
-  console.log('done !!!')
-})()
+  console.log('done !!!');
+})();
 
 async function waitUntil(id, expectState){
   while(true){
-    const result = await aliyun.invoke('DescribeInstanceAttribute', {InstanceId: id})
+    const result = await aliyun.invoke('DescribeInstanceAttribute', {InstanceId: id});
     if(result.Status === expectState){
       return;
     } else {
-      console.log(id, 'wait 3s', expectState)
-      await wait(3000)
+      console.log(id, 'wait 3s', expectState);
+      await wait(3000);
     }
   }
 }
